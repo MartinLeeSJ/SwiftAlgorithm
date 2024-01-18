0a41e0903239444a4942243e48712ba6bab8a0d0
Seokjun Lee <tjr2922@gmail.com>
2024-01-19 01:48:05 +0900

    Time: 8 ms (20.18%), Space: 15.3 MB (36.70%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.swift
+++ 0118-pascals-triangle/0118-pascals-triangle.swift
@@ -4,8 +4,9 @@ class Solution {
 
         for row in 0..<triangle.count {
             let halfOfRowSize: Double = Double(row + 1) / Double(2)
-            let limit: Int = Int(ceil(halfOfRowSize))
-            for col in 0..<limit {
+            let rowIteratingLimit: Int = Int(ceil(halfOfRowSize))
+            
+            for col in 0..<rowIteratingLimit {
                 if col == 0 { 
                     triangle[row][col] = 1 
                     continue
@@ -14,8 +15,8 @@ class Solution {
                 triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
             }
             
-            let appendLimit: Int = (row + 1) % 2 == 0 ? Int(limit) : Int(limit) - 1
-            let forward: Array<Int> = Array(triangle[row][0..<Int(limit)])
+            let appendLimit: Int = (row + 1) % 2 == 0 ? Int(rowIteratingLimit) : Int(rowIteratingLimit) - 1
+            let forward: Array<Int> = Array(triangle[row][0..<Int(rowIteratingLimit)])
             let backward: Array<Int> = Array(triangle[row][0..<appendLimit].reversed())
 
             triangle[row] = forward + backward
