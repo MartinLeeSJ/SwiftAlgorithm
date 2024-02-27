9cf61aef6257f2f9c32ff29591d7f08ecff2a205
Seokjun Lee <tjr2922@gmail.com>
2024-02-27 23:11:34 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 100 ms, Memory: 87324 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let T = Int(readLine()!)!
+
+var dp = Array(repeating: 0, count: 1_000_001)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+var answer = ""
+
+for _ in (1...T) {
+    let n = Int(readLine()!)!
+    
+    if n <= 3 {
+        answer += "\(dp[n])\n"
+        continue
+    }
+    
+    for i in (4...n) {
+        if dp[i] == 0 {
+           dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009
+        }
+    }
+    
+    answer += "\(dp[n] % 1_000_000_009)\n"
+    
+}
+
+print(answer)
